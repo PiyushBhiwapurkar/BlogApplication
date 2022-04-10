d37cbeaee24e19abf4ddf8f7244a850207a53828
PIYUSH BHIWAPURKAR <35625860+PiyushBhiwapurkar@users.noreply.github.com>
2022-04-10 18:42:48 +0530

    Updated ModelMapper Functionality in Service Implementation Class.

--- src/main/java/com/codewithpiyush/blog/Services/impl/userServiceImplementation.java
+++ src/main/java/com/codewithpiyush/blog/Services/impl/userServiceImplementation.java
@@ -3,6 +3,7 @@ package com.codewithpiyush.blog.Services.impl;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,9 @@ public class userServiceImplementation implements UserService {
 
 	@Autowired
 	private UserRepo userRepo; 
+	
+	@Autowired
+	private ModelMapper modelMapper;
 	
 	@Override
 	public UserDto createUser(UserDto userDto) {
@@ -61,23 +65,25 @@ public class userServiceImplementation implements UserService {
 	
 	private User dtoUserToUser(UserDto userDto)
 	{
-		User user = new User();
-		user.setId(userDto.getId());
-		user.setName(userDto.getName());
-		user.setEmail(userDto.getEmail());
-		user.setPassword(userDto.getPassword());
-		user.setAbout(userDto.getAbout());
+		User user =  this.modelMapper.map(userDto, User.class);
+		
+//		user.setId(userDto.getId());
+//		user.setName(userDto.getName());
+//		user.setEmail(userDto.getEmail());
+//		user.setPassword(userDto.getPassword());
+//		user.setAbout(userDto.getAbout());
 		return user;
 	}
 
 	private UserDto userToUserDto(User user)
 	{
-		UserDto userDto = new UserDto();
-		userDto.setId(user.getId());
-		userDto.setName(user.getName());
-		userDto.setEmail(user.getEmail());
-		userDto.setPassword(user.getPassword());
-		userDto.setAbout(user.getAbout());
+		UserDto userDto = this.modelMapper.map(user, UserDto.class);
+		
+//		userDto.setId(user.getId());
+//		userDto.setName(user.getName());
+//		userDto.setEmail(user.getEmail());
+//		userDto.setPassword(user.getPassword());
+//		userDto.setAbout(user.getAbout());
 		
 		return userDto;
 	}
