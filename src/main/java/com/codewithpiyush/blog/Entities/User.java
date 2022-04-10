6395bcac8c89a70ab9583272d23826aac0006c1a
PIYUSH BHIWAPURKAR <35625860+PiyushBhiwapurkar@users.noreply.github.com>
2022-04-10 15:09:19 +0530

    Database Configuration Update.

--- src/main/java/com/codewithpiyush/blog/Entities/User.java
+++ src/main/java/com/codewithpiyush/blog/Entities/User.java
@@ -7,8 +7,15 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
 @Table(name = "Users")
+@Getter
+@Setter
+@NoArgsConstructor
 public class User {
 
 	@Id
